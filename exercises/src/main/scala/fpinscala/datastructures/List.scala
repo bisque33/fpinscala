38ebdaac8d6913cd25b17147255dcc73ef676b0c
bisque33 <bisque33@gmail.com>
2017-11-02 02:08:36 +0900

    add exer3.1~3

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -5,7 +5,17 @@ case object Nil extends List[Nothing] // A `List` data constructor representing 
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
 which may be `Nil` or another `Cons`.
  */
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
+case class Cons[+A](head: A, tail: List[A]) extends List[A] {
+  // NOTE: toString()をカスタマイズしたい
+  override def toString: String = {
+    def go(head: A, tail: List[A], str: String): String = tail match {
+      case Nil => str + head + ")"
+      case _ => go(List.head(tail), List.tail(tail), str + head.toString + ",")
+    }
+
+    go(head, tail, "List(")
+  }
+}
 
 object List { // `List` companion object. Contains functions for creating and working with lists.
   def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
@@ -50,9 +60,21 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, tail) => tail
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  // TODO: headだけを取り出したい。戻り値の方がAだとNilが返せない
+  def head[A](l: List[A]): A = l match {
+    case Nil => sys.error("head on empty list")
+    case Cons(head, _) => head
+  }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, tail) => Cons(h, tail)
+  }
 
   def drop[A](l: List[A], n: Int): List[A] = ???
 
@@ -65,4 +87,44 @@ object List { // `List` companion object. Contains functions for creating and wo
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
+}
+
+object ListTest {
+
+  import List._
+
+  // test implementation of `fib`
+  def main(args: Array[String]): Unit = {
+    // exercise 3.1
+//    val x = List(1,2,3,4,5) match {
+//      case Cons(x, Cons(2, Cons(4, _))) => x
+//      case Nil => 42
+//      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+//      case Cons(h, t) => h + sum(t)
+//      case _ => 101
+//    }
+//    println(x)
+
+    // exercise 3.2
+//    val list = List(1,2,3,4,5)
+//    println("Expected: List(2,3,4,5)")
+//    println("Actual:   %s".format(List.tail(list)))
+//    val list = List(1)
+//    println("Expected: Nil")
+//    println("Actual:   %s".format(List.tail(list)))
+//    val list = Nil
+//    println("Expected: Nil")
+//    println("Actual:   %s".format(List.tail(list)))
+
+    // exercise 3.3
+//    val list = List(1,2,3,4,5)
+//    println("Expected: List(0,2,3,4,5)")
+//    println("Actual:   %s".format(List.setHead(list, 0)))
+
+
+//    println("Expected: true")
+//    println("Actual:   %s".format(isSorted(Array(0, 1, 2), (a: Int, b: Int) => a <= b)))
+//    println("Expected: false")
+//    println("Actual:   %s".format(isSorted(Array(0, 2, 1), (a: Int, b: Int) => a <= b)))
+  }
 }
