3ee33daaac73c36b14bb03bfd6f3c0ba8f97d631
cm-igarashi-ryosuke <igarashi.ryosuke@classmethod.jp>
2018-01-17 12:40:52 +0900

    勉強会のフィードバック

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,13 +6,17 @@ case class Leaf[A](value: A) extends Tree[A]
 
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
+// list<A> = Nil | Cons(a: A, b: List<A>)
+// データ型が、データコンストラクタ | が和
+// コンストラクタが、その引数の積
+// Ocamlの例がわかりやすい。
 
 object Tree {
 
   // exercise 3.25
   def size[A](t: Tree[A]): Int = t match {
     case leaf: Leaf[A] => 1
-    case branch: Branch[A] => size(branch.left) + size(branch.right)
+    case Branch(left,right) => size(left) + size(right) + 1
   }
 
   // exercise 3.26
@@ -33,7 +37,8 @@ object Tree {
     case branch: Branch[A] => Branch(map(branch.left)(f), map(branch.left)(f))
   }
 
-  def fold
+  // exercise 3.29
+  // def fold
 }
 
 
@@ -44,38 +49,38 @@ object TreeTest {
   def main(args: Array[String]): Unit = {
 
     // exercise 3.25
-    //    val tree1 = Leaf(1)
-    //    println("Expected: 1")
-    //    println("Actual:   %s".format(Tree.size(tree1)))
-    //
-    //    val tree2 = Branch(Branch(Leaf(1), Leaf(2)),Branch(Leaf(3), Leaf(4)))
-    //    println("Expected: 4")
-    //    println("Actual:   %s".format(Tree.size(tree2)))
+      //  val tree1 = Leaf(1)
+      //  println("Expected: 1")
+      //  println("Actual:   %s".format(Tree.size(tree1)))
+    
+      //  val tree2 = Branch(Branch(Leaf(1), Leaf(2)),Branch(Leaf(3), Leaf(4)))
+      //  println("Expected: 7")
+      //  println("Actual:   %s".format(Tree.size(tree2)))
 
     // exercise 3.26
-    //    val tree1 = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
-    //    println("Expected: 4")
-    //    println("Actual:   %s".format(Tree.size(tree1)))
-    //
-    //    val tree2 = Branch(Branch(Leaf(4), Leaf(3)), Branch(Leaf(2), Leaf(1)))
-    //    println("Expected: 4")
-    //    println("Actual:   %s".format(Tree.size(tree2)))
+       val tree1 = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+       println("Expected: 4")
+       println("Actual:   %s".format(Tree.maximum(tree1)))
+    
+       val tree2 = Branch(Branch(Leaf(4), Leaf(3)), Branch(Leaf(2), Leaf(1)))
+       println("Expected: 4")
+       println("Actual:   %s".format(Tree.maximum(tree2)))
 
     // exercise 3.27
-    val tree1 = Leaf(1)
-    println("Expected: 1")
-    println("Actual:   %s".format(Tree.depth(tree1)))
+    // val tree1 = Leaf(1)
+    // println("Expected: 1")
+    // println("Actual:   %s".format(Tree.depth(tree1)))
 
-    val tree2 = Branch(Leaf(1), Leaf(2))
-    println("Expected: 2")
-    println("Actual:   %s".format(Tree.depth(tree2)))
+    // val tree2 = Branch(Leaf(1), Leaf(2))
+    // println("Expected: 2")
+    // println("Actual:   %s".format(Tree.depth(tree2)))
 
-    val tree3 = Branch(Branch(Leaf(4), Leaf(3)), Branch(Leaf(2), Leaf(1)))
-    println("Expected: 3")
-    println("Actual:   %s".format(Tree.depth(tree3)))
+    // val tree3 = Branch(Branch(Leaf(4), Leaf(3)), Branch(Leaf(2), Leaf(1)))
+    // println("Expected: 3")
+    // println("Actual:   %s".format(Tree.depth(tree3)))
 
-    val tree4 = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Branch(Leaf(4), Leaf(5))))
-    println("Expected: 4")
-    println("Actual:   %s".format(Tree.depth(tree4)))
+    // val tree4 = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Branch(Leaf(4), Leaf(5))))
+    // println("Expected: 4")
+    // println("Actual:   %s".format(Tree.depth(tree4)))
   }
 }
